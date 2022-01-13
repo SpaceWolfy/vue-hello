3526cee57890a859d9a655800bdeab1ec21db407
Samuel Scozzari <s_scozzari@libero.it>
2022-01-13 17:58:32 +0100

    Added keyup function on pression of enter key

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@ new Vue({
   el: "#app", //id preso dall'HTML
   data: {
     message:
-      "Benvenuto, questo è il primo esercizio che ho realizzato utilizzando Vue.js! Prima di iniziare, come ti chiami?",
+      "Benvenuto, questo è il primo esercizio che ho realizzato utilizzando Vue.js! Prima di iniziare, inserisci il tuo nom, successivamente premi invio!",
     userName: "",
     imageSet: "https://c.tenor.com/lnzb_NOYs1cAAAAC/baby-yoda.gif",
+  },
+  methods: {
+    submitValue: function (e) {
+      this.userName = e.target.value;
+      e.target.value = "";
+
+      let outputHtml = document.querySelector(".str1");
+      outputHtml.innerHTML = `Ciao ${this.userName}, bella questa gif, vero?`;
+    },
   },
 });
